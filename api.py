d5b1727edfb50d25923964cfa4bffcee24665ef6
MarieMichel05 <pro.marie.michel@gmail.com>
2024-02-01 15:29:27 +0100

    correct api and dashboard

--- api.py
+++ api.py
@@ -11,11 +11,22 @@ with open('utils/model.pkl', 'rb') as model_file:
 with open('utils/scaler.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
-
 @app.route('/')
 def home_page():
-    return 'welcome to the credit scoring api'
+    return 'Welcome to the credit scoring API'
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json(force=True)
+        test_data = np.array(data['test_data'])
+        scaled_data = scaler.transform(test_data)
+        prediction = model.predict_proba(scaled_data)[:, 1]  # Assuming a binary classification task
+
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
